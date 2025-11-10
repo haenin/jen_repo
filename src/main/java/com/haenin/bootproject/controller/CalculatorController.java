20b22f157f7d1f1f550640f2cc15a37bae277d65
haenin <heyone0022@gmail.com>
2025-11-10 11:26:52 +0900

    finally

--- src/main/java/com/haenin/bootproject/controller/CalculatorController.java
+++ src/main/java/com/haenin/bootproject/controller/CalculatorController.java
@@ -25,7 +25,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm Alive! Good Weather jenkins! + Good Monday!";
+        return "I'm Alive! Good Weather jenkins! + Good Monday! + finally!!";
     }
 
 //    @GetMapping("/plus")
